1d0d681399e78e7f0b0516369377e394b5510dae
gio <56406617+mikoBerries@users.noreply.github.com>
2023-03-02 04:26:14 +0700

    decoupling code using interface

---
+++ decoupling_using_interface/main.go
@@ -0,0 +1,69 @@
+//decoupling for database func using interface
+package main
+
+import "fmt"
+
+type person struct {
+	first string
+}
+
+type mongo map[int]person
+type postg map[int]person
+
+// `m mongo` is a receiver
+// `n int, p person` is a key/value pair
+func (m mongo) save(n int, p person) {
+	m[n] = p
+}
+
+func (m mongo) retrieve(n int) person {
+	return m[n]
+}
+
+func (pg postg) save(n int, p person) {
+	pg[n] = p
+}
+
+func (pg postg) retrieve(n int) person {
+	return pg[n]
+}
+
+type accessor interface {
+	save(n int, p person)
+	retrieve(n int) person
+}
+
+func put(a accessor, n int, p person) {
+	a.save(n, p)
+}
+
+func get(a accessor, n int) person {
+	return a.retrieve(n)
+}
+
+func main() {
+	dbm := mongo{}
+	dbp := postg{}
+	p1 := person{
+		first: "Jenny",
+	}
+
+	p2 := person{
+		first: "James",
+	}
+
+	//store to mongo
+	put(dbm, 1, p1)
+	put(dbm, 2, p2)
+
+	fmt.Println(get(dbm, 1))
+	fmt.Println(get(dbm, 2))
+
+	//or store to db postgre
+	put(dbp, 1, p1)
+	put(dbp, 2, p2)
+
+	fmt.Println(get(dbp, 1))
+	fmt.Println(get(dbp, 2))
+
+}
