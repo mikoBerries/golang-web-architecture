b909c2341a492e5a4179f4ba40c9ec094d7d6ee7
gio <56406617+mikoBerries@users.noreply.github.com>
2023-03-06 02:25:29 +0700

    Hand on exercise 2 part 1

--- cmd/person/main.go
+++ cmd/person/main.go
@@ -1,43 +1,27 @@
-//decoupling for database func using interface
 package main
 
-import (
-	"fmt"
+import "fmt"
 
-	architecture "github.com/golang-web-architecture"
-	"github.com/golang-web-architecture/storage/mongo"
-	"github.com/golang-web-architecture/storage/postgres"
-)
+type hotdog int
+
+func (h hotdog) Cook() {
+	fmt.Println("Hotdog has been cooked")
+}
+
+type hotFood interface {
+	Cook()
+}
+
+func servingFood(hf hotFood) {
+	hf.Cook()
+	fmt.Println("serving hot hotdog")
+}
 
 func main() {
-	dbm := mongo.DB{}
-	dbp := postgres.DB{}
-	p1 := architecture.Person{
-		First: "Jenny",
-	}
+	var x hotdog = 42
+	var y hotFood
+	y = x
+	fmt.Printf("%T\n", y)
 
-	p2 := architecture.Person{
-		First: "James",
-	}
-	//using mongo 	>>>dbm
-	//using postgre >>>dbp
-	ps := architecture.NewPersonService(dbm)
-
-	//store person to mongo db
-	architecture.Put(dbm, 1, p1)
-	architecture.Put(dbm, 2, p2)
-
-	fmt.Println(architecture.Get(dbm, 1))
-	fmt.Println(architecture.Get(dbm, 3))
-
-	fmt.Println(ps.Get(1))
-	fmt.Println(ps.Get(3))
-
-	//or store person to postgre db
-	architecture.Put(dbp, 1, p1)
-	architecture.Put(dbp, 2, p2)
-
-	fmt.Println(architecture.Get(dbp, 1))
-	fmt.Println(architecture.Get(dbp, 2))
-
+	servingFood(x)
 }
--- doc.go
+++
@@ -1,3 +0,0 @@
-//this is documentation in doc.go file
-//architecture providing a controler of Person
-package architecture
