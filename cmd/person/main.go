8e66e9478f23546368c97eebe0ff870f21b0009c
gio <56406617+mikoBerries@users.noreply.github.com>
2023-03-08 23:52:17 +0700

    Hand on exercise #5

--- cmd/person/main.go
+++ cmd/person/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"runtime"
 	"time"
 )
 
@@ -20,16 +21,40 @@ func main() {
 		fmt.Println(s.GetUserId(ctx))
 	*/
 	//hand on exercise 4
+	/*
+		timeout := 2 * time.Second
+		ctx, cancelF := context.WithTimeout(context.Background(), timeout)
+		defer cancelF()
+		time.Sleep(1 * time.Second)
+		select {
+		case <-ctx.Done():
+			fmt.Println("exit")
+		default:
+			fmt.Println("finish")
+		}
+	*/
 
-	timeout := 2 * time.Second
-	ctx, cancelF := context.WithTimeout(context.Background(), timeout)
+	ctx, cancelF := context.WithCancel(context.Background())
 	defer cancelF()
-	time.Sleep(1 * time.Second)
-	select {
-	case <-ctx.Done():
-		fmt.Println("exit")
-	default:
-		fmt.Println("finish")
-	}
+	// defer time.Sleep(2 * time.Second)
+	for i := 0; i < 100; i++ {
+		go func() {
+			for {
+				select {
+				case <-ctx.Done():
+					runtime.Goexit()
+					// return
+				default:
+					fmt.Println("go routine now :", runtime.NumGoroutine())
+					time.Sleep(100 * time.Millisecond)
+				}
+			}
+		}()
 
+	}
+	time.Sleep(2 * time.Second)
+	cancelF()
+	fmt.Println("cancelF called ")
+	time.Sleep(1 * time.Second)
+	defer fmt.Println("last go routine:", runtime.NumGoroutine())
 }
