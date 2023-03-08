656b061210fe90649d65e378691537f4e4734442
gio <56406617+mikoBerries@users.noreply.github.com>
2023-03-08 23:05:57 +0700

    hand on exercise #4

--- cmd/person/main.go
+++ cmd/person/main.go
@@ -3,18 +3,33 @@ package main
 import (
 	"context"
 	"fmt"
-
-	s "github.com/golang-web-architecture/session"
+	"time"
 )
 
 func main() {
+	//hand on exercise 2
+	/*
+		ctx =context.Background()
+		defer cancelF()
+		ctx.Done()
+		// defer ctx.Done()
+		ctx = s.SetUserId(ctx, "1")
+		ctx = s.SetAdmin(ctx, true)
 
-	ctx := context.Background()
-	defer ctx.Done()
-	ctx = s.SetUserId(ctx, "1")
-	ctx = s.SetAdmin(ctx, true)
+		fmt.Println(s.GetAdmin(ctx))
+		fmt.Println(s.GetUserId(ctx))
+	*/
+	//hand on exercise 4
 
-	fmt.Println(s.GetAdmin(ctx))
-	fmt.Println(s.GetUserId(ctx))
+	timeout := 2 * time.Second
+	ctx, cancelF := context.WithTimeout(context.Background(), timeout)
+	defer cancelF()
+	time.Sleep(1 * time.Second)
+	select {
+	case <-ctx.Done():
+		fmt.Println("exit")
+	default:
+		fmt.Println("finish")
+	}
 
 }
