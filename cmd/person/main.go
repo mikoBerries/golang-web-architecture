3f685e6597e1f85093e7218a6e959aff238a128e
gio <56406617+mikoBerries@users.noreply.github.com>
2023-03-10 03:08:22 +0700

    error in depth

--- cmd/person/main.go
+++ cmd/person/main.go
@@ -4,7 +4,9 @@ import (
 	"errors"
 	"fmt"
 	"io/fs"
+	"log"
 	"net"
+	"os"
 )
 
 func main() {
@@ -29,12 +31,51 @@ func main() {
 	} else if errors.Is(fsNotExist.Unwrap(), fs.ErrNotExist) { //are this err not exist ?
 		fmt.Printf("file %v/%v Are not exist please do semothing", fsNotExist.Path, fsNotExist.Op)
 	}
-	var netErr *net.Error
-	if errors.As(err1, &netErr) { // are err1 is net.error?
-		//do something
-		// if err1.Timeout() {
+	var addErr *net.AddrError
+	if errors.As(err1, &addErr) { // are err1 is net.AddrError?
+		// do something
+		if addErr.Timeout() {
 
-		// }
+		}
 	}
-	//some toher eror
+	//some other eror
+
+	f, err := os.Open("rumi.txt")
+	var perr *os.PathError
+
+	switch {
+	case errors.Is(err, os.ErrPermission) && errors.As(err, &perr): // it is os err.permission erorr? && err is type os.PathError?
+		err = fmt.Errorf("you do not have permission to open the file: %w and the path is %s", err, perr.Path)
+		log.Println(err)
+	case errors.Is(err, os.ErrNotExist) && errors.As(err, &perr): // it is os err.ErrNotExist erorr? && err is type os.PathError?
+		err = fmt.Errorf("the file does not exist: %w and the path is %s", err, perr.Path)
+		log.Println(err)
+	case errors.As(err, &perr):
+		err = fmt.Errorf("here is the orignial error %w and the path %s", err, perr.Path)
+		log.Println(err)
+	case err != nil:
+		err = fmt.Errorf("file couldn't be opened: %w", err)
+		log.Println(err)
+	}
+	defer f.Close()
+
+	//unwrap help to getting depper detail error in diffrent class / method that produce eror massage
+	unwrapErr := foo()
+	fmt.Println(unwrapErr)
+}
+
+func cat() error {
+	return errors.New("cat is an error")
+}
+
+func moo() error {
+	return fmt.Errorf("moo is an error: %w", cat())
+}
+
+func bar() error {
+	return fmt.Errorf("bar is an error: %w", moo())
+}
+
+func foo() error {
+	return fmt.Errorf("foo is an error: %w", bar())
 }
