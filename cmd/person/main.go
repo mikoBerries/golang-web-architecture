659834b905838b15acd64c68d056b873541a3bb4
gio <56406617+mikoBerries@users.noreply.github.com>
2023-03-11 00:03:54 +0700

    Hand On Exercise#6

--- cmd/person/main.go
+++ cmd/person/main.go
@@ -22,14 +22,25 @@ func main() {
 	var errpatherr *os.PathError
 
 	//using error.Is() to make more meaning full error massage in cmd
-	if err != nil && errors.Is(err, os.ErrNotExist) {
+	if errors.Is(err, os.ErrNotExist) {
 		fmt.Printf("you need to provide the name \"%s\" of a valid file in your directory next to the executable - %s\n", fileName, errpatherr.Path)
 	} else if errors.As(err, &errpatherr) {
 		//using error.As() to get more detailed erorr massage from error struct
 		fmt.Printf("error in copyFile: %s - OPERATION: %s - %s\n", errpatherr.Path, errpatherr.Op, err)
-	} else {
+	} else if err != nil {
 		log.Fatal("in func main while calling handOnExercise3() error val :", err)
 	}
+
+	temperr := foo()
+	fmt.Println(temperr) //all
+	temperr = errors.Unwrap(temperr)
+	fmt.Println(temperr) //barr moo cat
+	temperr = errors.Unwrap(temperr)
+	fmt.Println(temperr) // moo cat
+	temperr = errors.Unwrap(temperr)
+	fmt.Println(temperr) // cat
+	temperr = errors.Unwrap(temperr)
+	fmt.Println(temperr) //nil
 
 }
 func handOnExercise3(fileName string, createFile string) error {
@@ -52,3 +63,8 @@ func handOnExercise3(fileName string, createFile string) error {
 
 	return nil
 }
+
+func foo() error { return fmt.Errorf("eror from foo \t %w", bar()) }
+func bar() error { return fmt.Errorf("eror from bar \t %w", moo()) }
+func moo() error { return fmt.Errorf("eror from moo \t %w", cat()) }
+func cat() error { return errors.New("eror from cat \t") }
