6348771bd3908a529a9e9cff91d77f950571fc69
gio <56406617+mikoBerries@users.noreply.github.com>
2023-03-10 23:54:14 +0700

    Hand on Exercise #5

--- cmd/person/main.go
+++ cmd/person/main.go
@@ -19,10 +19,14 @@ func main() {
 	fileName := "this.txt"
 	createFile := "createthis.txt"
 	err := handOnExercise3(fileName, createFile)
+	var errpatherr *os.PathError
 
-	//using error is to make more meaning full error massage in cmd
+	//using error.Is() to make more meaning full error massage in cmd
 	if err != nil && errors.Is(err, os.ErrNotExist) {
-		fmt.Println("Error: file not found")
+		fmt.Printf("you need to provide the name \"%s\" of a valid file in your directory next to the executable - %s\n", fileName, errpatherr.Path)
+	} else if errors.As(err, &errpatherr) {
+		//using error.As() to get more detailed erorr massage from error struct
+		fmt.Printf("error in copyFile: %s - OPERATION: %s - %s\n", errpatherr.Path, errpatherr.Op, err)
 	} else {
 		log.Fatal("in func main while calling handOnExercise3() error val :", err)
 	}
@@ -45,5 +49,6 @@ func handOnExercise3(fileName string, createFile string) error {
 		return fmt.Errorf("error while copy file %s: %w", fileName, err)
 	}
 	fmt.Println("writen data :", n)
+
 	return nil
 }
