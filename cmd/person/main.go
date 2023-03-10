d0a2be267ef2d105b701e28b6c9b6a69558840bc
gio <56406617+mikoBerries@users.noreply.github.com>
2023-03-10 22:36:38 +0700

    hand ond exercise #3

--- cmd/person/main.go
+++ cmd/person/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"os"
+)
 
 type meow int
 
@@ -10,4 +14,29 @@ func (m meow) error() string {
 func main() {
 	var m meow = 1
 	fmt.Println(m.error())
+	fileName := "this.txt"
+	createFile := "createthis.txt"
+	err := handOnExercise3(fileName, createFile)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
+func handOnExercise3(fileName string, createFile string) error {
+	f, err := os.Open(fileName)
+	if err != nil {
+		return fmt.Errorf("error while opening file %s: %w", fileName, err)
+	}
+	defer f.Close()
+	cf, err := os.Create(createFile)
+	if err != nil {
+		return fmt.Errorf("error while creating file %s: %w", createFile, err)
+	}
+	defer cf.Close()
+
+	n, err := io.Copy(cf, f)
+	if err != nil {
+		return fmt.Errorf("error while copy file %s: %w", fileName, err)
+	}
+	fmt.Println("writen data :", n)
+	return nil
 }
